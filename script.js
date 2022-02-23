862f042303da51bd37a969e63842eaefaa16ed60
rdlooney <96710687+rdlooney@users.noreply.github.com>
2022-02-22 22:55:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+f = function (x) {return -4 * x + 2}
+g = function (x) {return x * x / 25}
+
+function update() {
+  x = parseFloat(document.getElementById('x').value)
+  h = parseFloat(document.getElementById('h').value)
+  document.getElementById('fx').innerHTML = `f(x) = ${f(x).toFixed(3)}`
+  document.getElementById('df').innerHTML =
+    `f(x+h) - f(x)= ${(f(x+h)-f(x)).toFixed(3)}`
+  document.getElementById('gx').innerHTML = `g(x) = ${g(x).toFixed(3)}`
+  document.getElementById('dg').innerHTML =
+    `g(x+h) - g(x)= ${(g(x+h)-g(x)).toFixed(3)}`
+}
+
